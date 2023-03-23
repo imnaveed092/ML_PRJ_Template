c2205dcb0cc298a9624bc570e6119b042d27adb7
imnaveed092 <imnaveed000@gmail>
2023-03-23 20:12:58 +0530

    data ingestion completed

--- Visa/entity/config_entity.py
+++ Visa/entity/config_entity.py
@@ -1,9 +1,11 @@
+
 from collections import namedtuple
 
 
+DataIngestionConfig=namedtuple("DataIngestionConfig",
+["dataset_download_url","raw_data_dir","ingested_train_dir","ingested_test_dir"])
 
-DataIngestionConfig: namedtuple("DataIngestionConfig",
-                                ['dataset_download_url','raw_data_dir','ingested_dir','ingetsted_train_dir','ingested_test_dir'])
 
+DataValidationConfig = namedtuple("DataValidationConfig", ["schema_file_path"])
 
-DataValidationCongif: namedtuple('DataValidationCongif',['schema_file_dir'])
+TrainingPipelineConfig = namedtuple("TrainingPipelineConfig", ["artifact_dir"])
